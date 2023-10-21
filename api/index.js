33b4b2aaa018d621d583ccaa2f0f8d5544f0668c
Rohan Singh <rohan704532@gmail.com>
2023-10-21 22:25:10 +0530

    complete signup page UI

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config()
 
 mongoose
@@ -13,6 +14,7 @@ mongoose
     console.log(err)
 })
 const app = express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log("Service is runnoing on 3000!!")
@@ -20,3 +22,14 @@ app.listen(3000,()=>{
 
 
 app.use("/api/user",userRouter);
+app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
