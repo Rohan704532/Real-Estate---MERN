cab145c68849e3c1a41788d15c783ef63bd6fe24
Rohan Singh <rohan704532@gmail.com>
2023-10-21 21:20:45 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
 
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to MongoDB")
+})
+.catch((err)=>{
+    console.log(err)
+})
 const app = express()
 
 app.listen(3000,()=>{
     console.log("Service is runnoing on 3000!!")
 })
+
+// /Wy9veMN8dip3PzvK
