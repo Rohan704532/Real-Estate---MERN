8237e07ffc73f2b6717b3ac02db51f5c716699b8
Rohan Singh <rohan704532@gmail.com>
2023-10-22 14:13:47 +0530

    profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -54,7 +54,8 @@ export const google = async (req,res,next)=>{
                  avatar:req.body.photo
                 })
             await newUser.save()
-            const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET)
+            const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+            const {password:pass,...rest} = newUser._doc
             res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
         }
     } catch (error) {
